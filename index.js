e49381d4012aeba6dc10a433c637345d279dbc1c
Vanv1k <ivanspecial5@gmail.com>
2022-04-15 19:04:10 +0300

    commit

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.json())
 app.use(stockRoutes);
 app.use(express.static('views/images'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 
 
 async function start() {
